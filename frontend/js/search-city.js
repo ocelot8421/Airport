9bed887fbee92ead96e13409d2138c7c97b55b4b
ocelot8421 <8ocelot@gmail.com>
2023-06-29 14:07:36 +0200

    wikiDataId implemented at frontend

--- frontend/js/search-city.js
+++ frontend/js/search-city.js
@@ -3,24 +3,36 @@ const cityJSON = {
     name: '',
     coordinates: { long: null, width: null }
 }
+const cityList = {
+    London: { name: 'London', wikiDataId: 'Q84' },
+    New_Delhi: { name: 'New Delhi', wikiDataId: 'Q987' },
+    New_York: { name: 'New York', wikiDataId: 'Q60' },
+    Tokyo: { name: 'Tokyo', wikiDataId: 'Q1490' },
+    Paris: { name: 'Paris', wikiDataId: 'Q90' }
+}
 
 /*
-Add search function to "Search" button.
-At first, it gets the name of selected city then appears above the button
+Add search function to "Search" buttun.
+At first, it gets the name of selected city then appers above the button 
 at the end of "Selected:" line on the webpage. Afterwards, it is sent to backend.
 */
 searchBtn.addEventListener("click", (e) => {
     let city = getSelectedCity();
     sendCityToBackend(city);
-
 })
 
 /*
-It gets the name of selected city wait is appears above the button at "Selected:" line on the webpage.
+It gets the name of selected city waht is appers above the button at "Selected:" line on the webpage.
 */
 function getSelectedCity() {
     const selectElement = document.querySelector('#select1');
     const output = selectElement.value;
+
+    for (let e in cityList) {
+        if (output == `${cityList[e].name}`)
+            cityJSON.wikiDataId = `${cityList[e].wikiDataId}`;
+    }
+
     document.querySelector('.output').textContent = output;
     return output;
 }
